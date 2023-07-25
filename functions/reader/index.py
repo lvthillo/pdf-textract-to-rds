07c59cbd34c71124b87d707a7e5838244d5b90a9
lvthillo <lorenz.vanthillo@outlook.com>
2023-07-25 21:03:41 +0200

    Fix table bug + remove layer dependency + upgrade Postgres and lambda runtimes.

--- functions/reader/index.py
+++ functions/reader/index.py
@@ -1,34 +1,33 @@
-import json
 import os
+import json
 import boto3
 
-def lambda_handler(event, context):
+
+def lambda_handler(event, _):
     print("Triggered getTextFromS3PDF event: " + json.dumps(event, indent=2))
 
     # Get the object from the event and show its content type
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    key = event['Records'][0]['s3']['object']['key']
+    bucket = event["Records"][0]["s3"]["bucket"]["name"]
+    key = event["Records"][0]["s3"]["object"]["key"]
 
     try:
-        textract = boto3.client('textract')
+        textract = boto3.client("textract")
         textract.start_document_analysis(
-        DocumentLocation={
-            'S3Object': {
-                'Bucket': bucket,
-                'Name': key
-            }
-        },
-        JobTag=key + '_Job',
-        FeatureTypes=[
-            'TABLES'
-        ],
-        NotificationChannel={
-            'RoleArn': os.environ['SNSROLEARN'],
-            'SNSTopicArn': os.environ['SNSTOPIC']
-        })
-        
-        return 'Triggered PDF Processing for ' + key
+            DocumentLocation={"S3Object": {"Bucket": bucket, "Name": key}},
+            JobTag=key + "_Job",
+            FeatureTypes=["TABLES"],
+            NotificationChannel={
+                "RoleArn": os.environ["SNSROLEARN"],
+                "SNSTopicArn": os.environ["SNSTOPIC"],
+            },
+        )
+
+        return "Triggered PDF Processing for " + key
     except Exception as e:
         print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        print(
+            "Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.".format(
+                key, bucket
+            )
+        )
         raise e
